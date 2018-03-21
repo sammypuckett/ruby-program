7c738bdf18568ac7444d3315c6458e9b5713d81a
Alex <acsouthwick@gmail.com>
2018-03-21 09:41:59 -0700

    New stuff

--- rubyproject/config/routes.rb
+++ rubyproject/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 	get 'welcome' => 'pages#home'
+
+	resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
